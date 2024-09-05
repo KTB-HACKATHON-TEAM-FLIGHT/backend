ed4f20e49aca0ba5ac089290b54675d4c5f6faa5
youth <boysoeng@naver.com>
2024-09-05 20:24:04 +0900

    docs: 테스트 API 설명 추가

--- src/main/java/team/flight/backend/controller/TestController.java
+++ src/main/java/team/flight/backend/controller/TestController.java
@@ -1,5 +1,7 @@
 package team.flight.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import team.flight.backend.dto.FirstPostRequest;
 import team.flight.backend.service.dto.FirstResponse;
 
+@Tag(name = "TEST 관련 API")
 @RestController
 @RestControllerAdvice
 @RequestMapping("/api/test")
 public class TestController {
 
+    @Operation(summary = "TEST 요청 API")
     @PostMapping
     public ResponseEntity<FirstResponse> testApi(@RequestBody FirstPostRequest request){
         return ResponseEntity.ok().body(new FirstResponse(1L, request.request()));
