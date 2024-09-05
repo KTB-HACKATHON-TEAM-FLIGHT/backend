424a669fb5b4e24929efb8f373a9ff7fbb4d9b0d
youth <boysoeng@naver.com>
2024-09-05 20:37:28 +0900

    feat: ping 체크용 api 추가

--- src/main/java/team/flight/backend/controller/TestController.java
+++ src/main/java/team/flight/backend/controller/TestController.java
@@ -3,6 +3,7 @@ package team.flight.backend.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,11 @@ public class TestController {
     @PostMapping
     public ResponseEntity<FirstResponse> testApi(@RequestBody FirstPostRequest request){
         return ResponseEntity.ok().body(new FirstResponse(1L, request.request()));
+    }
+
+    @Operation(summary = "서버 상태 확인용 API")
+    @GetMapping
+    public ResponseEntity<String> pingApi(){
+        return ResponseEntity.ok().body("OK");
     }
 }
