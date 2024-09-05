f8938e799200e3c6e27dad3d942c5cb83a595187
youth <boysoeng@naver.com>
2024-09-05 15:37:03 +0900

    chore: 전역 예외 처리 핸들러 추가

---
+++ src/main/java/team/flight/backend/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package team.flight.backend.global.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
