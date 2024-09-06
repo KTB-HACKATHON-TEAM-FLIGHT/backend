ec01b626444dfbe488ad730cd11d2655f979a2fd
youth <boysoeng@naver.com>
2024-09-06 17:31:56 +0900

    feat: swagger server domain

--- src/main/java/team/flight/backend/BackendApplication.java
+++ src/main/java/team/flight/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package team.flight.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://api.chatppt.site")})
 public class BackendApplication {
 
     public static void main(String[] args) {
