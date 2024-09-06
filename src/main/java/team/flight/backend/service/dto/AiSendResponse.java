35ddbdd4212ccf4b66a2b4624b50c0d3553854b3
youth <boysoeng@naver.com>
2024-09-07 02:02:10 +0900

    delete constructor of ai send response

--- src/main/java/team/flight/backend/service/dto/AiSendResponse.java
+++ src/main/java/team/flight/backend/service/dto/AiSendResponse.java
@@ -1,10 +1,8 @@
 package team.flight.backend.service.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class AiSendResponse {
 
     private String response;
