9862e97f16cf73bc9db9b51b9696913fa23d8266
youth <boysoeng@naver.com>
2024-09-07 00:42:54 +0900

    feat: AI 서버 요청간 타임 아웃 시간 설정 및 예외 핸들링

--- src/main/java/team/flight/backend/global/exception/ErrorCode.java
+++ src/main/java/team/flight/backend/global/exception/ErrorCode.java
@@ -9,8 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, 4004, "해당 객체가 존재하지 않습니다."),
-    PDF_CONVERT_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, 5005, "파일을 PDF로 변환하는 과정에서 에러가 발생했습니다.")
+    PDF_CONVERT_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, 5005, "파일을 PDF로 변환하는 과정에서 에러가 발생했습니다."),
+    WEBCLIENT_CLIENT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5006, "AI 서버 요청 간 클라이언트 에러 발생"),
+    WEBCLIENT_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 5007, "AI 서버 요청 간 AI 서버 내부 에러 발생")
     ;
+
     private final HttpStatus httpStatus;
     private final int code;
     private final String message;
--- src/main/java/team/flight/backend/service/WebClientSender.java
+++ src/main/java/team/flight/backend/service/WebClientSender.java
@@ -1,8 +1,13 @@
 package team.flight.backend.service;
 
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import team.flight.backend.global.exception.AppException;
+import team.flight.backend.global.exception.ErrorCode;
 import team.flight.backend.service.dto.AiSendRequest;
 
 @Component
@@ -16,7 +21,12 @@ public class WebClientSender {
                 .uri("/api/ppt")
                 .bodyValue(AiSendRequest.from(request))
                 .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError,
+                        response -> Mono.error(() -> new AppException(ErrorCode.WEBCLIENT_SERVER_ERROR)))
+                .onStatus(HttpStatusCode::is4xxClientError,
+                        response -> Mono.error(() -> new AppException(ErrorCode.WEBCLIENT_CLIENT_ERROR)))
                 .bodyToMono(String.class)
+                .timeout(Duration.ofSeconds(30))
                 .block();
     }
 }
